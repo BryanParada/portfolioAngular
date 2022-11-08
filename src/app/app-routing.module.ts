d3f15f1a4bc48521dec76c16bbfc091302ddf50c
Bryan Parada <bryan.parada.cea@gmail.com>
2022-11-08 15:38:01 -0300

    Initial project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+     path:'home',
+     loadChildren: () =>  import('./portfolio/portfolio.module').then (m => m.PortfolioModule),
+  
+  },
+  {
+     path:'**',
+     redirectTo: 'home' 
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
